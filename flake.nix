44627e2d3c55f58364b3aa775946013142e4ed98
Mad <187168704+PkuseriHellish@users.noreply.github.com>
2026-08-06 20:44:31 +0800

    blender?

--- flake.nix
+++ flake.nix
@@ -2,22 +2,30 @@
   description = "NixOS configeee";
 
   inputs = {
-    # Swap this for a stable release branch (e.g. nixos-25.11) once one
-    # matching your stateVersion exists; unstable works fine either way.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    nixpkgs-blender36.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, nixpkgs-blender36, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
-        # home-manager.users.mad is defined directly inside configuration.nix,
-        # since configuration.nix is just another module merged into this set.
+        #poopgpt you better be right and that i get my blender 3.6
+        ({ pkgs, ... }: {
+          environment.systemPackages = [
+            (import nixpkgs-blender36 {
+              system = "x86_64-linux";
+              config.allowUnfree = true;
+            }).blender
+          ];
+        })
       ];
     };
   };
