5160611505c51988ddbc5d231c99dffb8e526fea
Mad <187168704+PkuseriHellish@users.noreply.github.com>
2026-08-07 12:19:13 +0800

    idfk

--- flake.nix
+++ flake.nix
@@ -2,28 +2,28 @@
   description = "NixOS configeee";
 
   inputs = {
-    # Swap this for a stable release branch (e.g. nixos-25.11) once one
-    # matching your stateVersion exists; unstable works fine either way.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-     nixos-hardware = {
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixos-hardware = {
       url = "github:NixOS/nixos-hardware";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }:
+  {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-
       system = "x86_64-linux";
+
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
         nixos-hardware.nixosModules.lenovo-thinkpad-t440s
-        # home-manager.users.mad is defined directly inside configuration.nix,
-        # since configuration.nix is just another module merged into this set.
       ];
     };
   };
