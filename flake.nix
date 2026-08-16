4a5117125464dd8a3b4e0eefc82b3b8bd59b8fab
Mad <187168704+PkuseriHellish@users.noreply.github.com>
2026-08-16 15:16:50 +0800

    acdc

--- flake.nix
+++ flake.nix
@@ -3,11 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    blender-bin.url = "github:edolstra/nix-warez?dir=blender";
 
-home-manager = {
-  url = "github:nix-community/home-manager/release-26.05";
-  inputs.nixpkgs.follows = "nixpkgs";
-};
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nixos-hardware = {
       url = "github:NixOS/nixos-hardware";
@@ -15,15 +16,22 @@ home-manager = {
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }:
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, blender-bin, ... }:
   {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit blender-bin; };
 
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
         nixos-hardware.nixosModules.lenovo-thinkpad-t440s
+
+        ({ blender-bin, pkgs, ... }: {
+          environment.systemPackages = [
+            blender-bin.packages.${pkgs.system}.blender_3_6
+          ];
+        })
       ];
     };
   };
